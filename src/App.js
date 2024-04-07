9ae6db9c838b7cb8b1fb7c43348db60e5804a0bb
minna <minnakrandolph@gmail.com>
2024-04-06 21:16:13 -0400

    added github repository link to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
      Weather App
+     <div>
+     <a href="https://github.com/minnakrandolph/4_6_24.react.weather.app" 
+     target="_blank">
+      Github repository</a> </div>
     </div>);
 }
 
