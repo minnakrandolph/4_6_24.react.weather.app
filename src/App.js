4669736e4149e987c7eb1f09492ebe9996063f1b
minna <minnakrandolph@gmail.com>
2024-04-07 08:01:45 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Weather App
+      <div className="container">
+    <h1>Weather App</h1> 
+     <button>hello</button>
      <footer>
       this project was created by Minna Randolph and is open sourced 
      <a href="https://github.com/minnakrandolph/4_6_24.react.weather.app" 
      target="_blank">
-      Github repository</a> </footer>
+      Github repository</a> 
+      </footer>
+      </div>
     </div>);
 }
 
