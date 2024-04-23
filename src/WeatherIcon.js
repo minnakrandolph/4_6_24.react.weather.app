e3f77e9bbdf474e367b01ff74c24a11d4d3a1b9c
minna <minnakrandolph@gmail.com>
2024-04-23 08:07:59 -0400

    Created one forecast day component
    made WeatherForecast.js and the corresponding CSS

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ import ReactAnimatedWeather from "react-animated-weather";
         <ReactAnimatedWeather
         icon={codeMapping[props.code]}
         color="orange"
-        size={52}
+        size={props.size}
         animate={true}
         />
     );
