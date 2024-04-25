35a7a90ee00395aff78b28ac0e3acdbe833ce060
minna <minnakrandolph@gmail.com>
2024-04-25 07:58:55 -0400

    set api for location in weather data
    api key, latitude, longitude, and apiUrl, to retrieve location data to apply to the daily forecast in WeatherForecast.js.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,23 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 
 
 export default function WeatherForecast(props) {
+function handleResponse(response) {
+    console.log(response.data);
+}
+
+
+    let apiKey = "3499ef150985eccadd080ff408a018df";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(handleResponse);
+
  return (
     <div className="WeatherForecast">
         <div className="row">
