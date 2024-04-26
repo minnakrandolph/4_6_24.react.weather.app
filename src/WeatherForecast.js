21ab8a9ee958085d9f86c28b99e5af4a7568d82d
minna <minnakrandolph@gmail.com>
2024-04-26 07:49:47 -0400

    Created forecast for 1st day
    created WeatherForecastDay, includeng daily data, weather temperature, and day of week

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,16 +1,32 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 
 
 export default function WeatherForecast(props) {
-function handleResponse(response) {
-    console.log(response.data);
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+
+
+    function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
 }
 
-
+if (loaded) {
+    console.log(forecast);
+    return (
+        <div className="WeatherForecast">
+            <div className="row">
+                <div className="col"> 
+                <WeatherForecastDay data={forecast[0]}/>
+                </div>
+            </div>
+            </div>
+    );
+} else {
     let apiKey = "3499ef150985eccadd080ff408a018df";
     let latitude = props.coordinates.lat;
     let longitude = props.coordinates.lon;
@@ -18,24 +34,8 @@ function handleResponse(response) {
 
 axios.get(apiUrl).then(handleResponse);
 
- return (
-    <div className="WeatherForecast">
-        <div className="row">
-            <div className="col"> 
-            <div className="WeatherForecast-day">Wednesday</div> 
-            
-            <WeatherIcon code="01d" size={36}/>
-             <div className="WeatherForecast-temperatures">
-                <span className="WeatherForecast-temperature-max">
-                    19° /
-                </span>
-                <span className="WeatherForecast-temperature-min">
-                    10°
-                </span>
-                </div>
-            </div>
-        </div>
-        </div>
- );
 
+return null;
+
+}
 }
